fe9dc1b9029c05235be5253beae34c47c4a13a3f
Zach <zacharyaclark@gmail.com>
2018-04-30 21:27:31 -0500

    Fork this one sherag

--- MainActivity.java
+++ MainActivity.java
@@ -3,12 +3,26 @@ package com.example.yaboyzc.oxforddictionary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
